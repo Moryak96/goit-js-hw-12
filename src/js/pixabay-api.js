a3f8ddf35c63ffdbd9a9cc9043b26a4a26fda546
Олександр <147379949+Moryak96@users.noreply.github.com>
2024-04-03 14:43:50 +0300

    Fix key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@ export async function getImage(query, currentPage) {
     const BASE_URL = 'https://pixabay.com/api/';
     const url = `${BASE_URL}`;
     const params = {
-        key: "43094029-935243e587570d50c5a59b02",
+        key: "43094029-935243e587570d50c5a59b02c",
         q: query,
         image_type: "photo",
         orientation: "horizontal",
